113eb4baab53e18d5aabd3f049c1510dcf554bb4
Priyanshu Raj <er.priyansu@gmail.com>
2024-10-20 20:54:10 +0530

    Create Binary_To_Decimal.java

---
+++ Algorithms/Binary_To_Decimal.java
@@ -0,0 +1,22 @@
+public class Binary_To_Decimal {
+
+    public static void binTodec(int binNum) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+
+        while (binNum > 0) {
+            int lastDigit = binNum % 10; 
+            decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+
+            pow++;
+            binNum = binNum/10;
+        }
+        System.out.println("Decimal of "+ myNum +" = "+ decNum);
+
+    }
+    
+    public static void main(String[] args) {
+        binTodec(1010); 
+    }
+}
